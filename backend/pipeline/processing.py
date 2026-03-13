1b5e7505c3cf27b03d5c04d16d8b275d67e194b9
rohinipandhare12 <rohinipandhare14@gmail.com>
2026-03-13 10:38:38 +0530

    Update processing.py

--- backend/pipeline/processing.py
+++ backend/pipeline/processing.py
@@ -1,21 +1,3 @@
-#why pipeline?
-#this pipeline injects the raw log data using dask bag. parse them into structured data with the help of dask data frame, we can filter the invalid log entries and converts into data frame using meta data.
-
-#why meta data?
-#dask cannot guess the column type automatically , we tell dask what is key and what is the value(passing key and value pairs) like what are column names and data types of the particular column.
-
-#why objects?
-#raw log data are strings intially ,which are in text files, using objects it is safer browser for better understanding
-
-
-# example
-# meta_data = {
-#     # pandas/dask expects a valid dtype; use datetime64 for timestamp
-#     "timestamp": "datetime64[ns]", 
-#     "level" : "string", 
-#     "service" : "string", 
-#     "message" : "string",
-# }
 
 import dask.dataframe as dd
 from backend.ingestion.parser import parse_log_line
@@ -25,7 +7,7 @@ def process_pipeline(file_path):
     bag=load_logs(file_path)
     parsed = (
         bag.map(parse_log_line)
-        .filter(lambda x: x is not None)  # Filter out lines that failed to parse
+        .filter(lambda x: x is not None)  
     )
     meta_data = {
         "timestamp": "datetime64[ns]", 
@@ -34,5 +16,5 @@ def process_pipeline(file_path):
         "message" : "string",
     }
     df = parsed.to_dataframe(meta=meta_data)
-    df["timestamp"] = dd.to_datetime(df["timestamp"], errors='coerce')       #convert to datetime, coerce errors to NaT
+    df["timestamp"] = dd.to_datetime(df["timestamp"], errors='coerce')       
     return df
